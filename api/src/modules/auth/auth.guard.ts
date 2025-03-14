39182f25ee119c141931118286722775169f5ea2
Tonysw2 <anthonyrmataa@gmail.com>
2025-03-14 09:48:18 -0300

    refactor(be): change the description of token validation errors

--- api/src/modules/auth/auth.guard.ts
+++ api/src/modules/auth/auth.guard.ts
@@ -33,7 +33,7 @@ export class AuthGuard implements CanActivate {
     const token = this.extractTokenFromHeader(request);
 
     if (!token) {
-      throw new UnauthorizedException();
+      throw new UnauthorizedException('Missing token.');
     }
 
     try {
@@ -43,7 +43,7 @@ export class AuthGuard implements CanActivate {
 
       request['user'] = payload;
     } catch {
-      throw new UnauthorizedException();
+      throw new UnauthorizedException('Invalid token.');
     }
 
     return true;
