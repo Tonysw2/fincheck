8656a7c221546753ce730fd340f47f7094ac33c2
Tonysw2 <anthonyrmataa@gmail.com>
2025-03-20 15:54:17 -0300

    feat: add logout option on user menu

---
+++ frontend/src/view/components/dropdown-menu.tsx
@@ -0,0 +1,58 @@
+import * as RadixDropdownMenu from '@radix-ui/react-dropdown-menu'
+
+import { cn } from '../../app/utils/cn'
+
+export function DropdownMenuRoot(props: RadixDropdownMenu.DropdownMenuProps) {
+  return <RadixDropdownMenu.Root {...props} />
+}
+
+export function DropdownMenuTrigger({
+  className,
+  ...props
+}: RadixDropdownMenu.DropdownMenuTriggerProps) {
+  return (
+    <RadixDropdownMenu.Trigger
+      className={cn('outline-none', className)}
+      {...props}
+    />
+  )
+}
+
+export function DropdownMenuContent({
+  className,
+  ...props
+}: RadixDropdownMenu.DropdownMenuContentProps) {
+  return (
+    <RadixDropdownMenu.Portal>
+      <RadixDropdownMenu.Content
+        className={cn(
+          'data-[side=bottom]:animate-slide-up-and-fade rounded-2xl bg-white p-2 shadow-[0px_11px_20px_0px_rgba(0,0,0,0.10)]',
+          className,
+        )}
+        {...props}
+      />
+    </RadixDropdownMenu.Portal>
+  )
+}
+
+export function DropdownMenuItem({
+  className,
+  ...props
+}: RadixDropdownMenu.DropdownMenuItemProps) {
+  return (
+    <RadixDropdownMenu.Item
+      className={cn(
+        'flex min-h-12 cursor-pointer items-center justify-between rounded-lg bg-white p-2 text-sm text-gray-800 transition-colors outline-none data-[highlighted]:bg-gray-100',
+        className,
+      )}
+      {...props}
+    />
+  )
+}
+
+export const DropdownMenu = {
+  Root: DropdownMenuRoot,
+  Trigger: DropdownMenuTrigger,
+  Content: DropdownMenuContent,
+  Item: DropdownMenuItem,
+}
--- frontend/src/view/components/user-menu.tsx
+++ frontend/src/view/components/user-menu.tsx
@@ -1,9 +1,26 @@
+import { LogOut } from 'lucide-react'
+
+import { useAuthContext } from '../../app/hooks/use-auth'
+import { DropdownMenu } from '../components/dropdown-menu'
+
 export function UserMenu() {
+  const { signOut } = useAuthContext()
+
   return (
-    <div className="flex size-12 items-center justify-center rounded-full border border-teal-100 bg-teal-50">
-      <span className="text-sm font-medium tracking-tight text-teal-900">
-        AR
-      </span>
-    </div>
+    <DropdownMenu.Root>
+      <DropdownMenu.Trigger className="outline-none">
+        <div className="flex size-12 items-center justify-center rounded-full border border-teal-100 bg-teal-50">
+          <span className="text-sm font-medium tracking-tight text-teal-900">
+            AR
+          </span>
+        </div>
+      </DropdownMenu.Trigger>
+
+      <DropdownMenu.Content align="end" className="w-32">
+        <DropdownMenu.Item className="gap-2" onClick={signOut}>
+          Sair <LogOut className="size-4" />
+        </DropdownMenu.Item>
+      </DropdownMenu.Content>
+    </DropdownMenu.Root>
   )
 }
